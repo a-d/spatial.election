1c00603feff40cc1e26ff376b9e02a44055a4a98
a-d <alexander_duemont@web.de>
2014-01-17 15:47:00 +0100

    changed geom output of Constituency to "coordinates" with 1/2/3-dimensionals double-arrays

--- spatial.election.database/src/main/java/edu/spatial/election/domain/Constituency.java
+++ spatial.election.database/src/main/java/edu/spatial/election/domain/Constituency.java
@@ -10,6 +10,7 @@ import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.hibernate.annotations.Type;
 
+import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Point;
 
@@ -40,12 +41,29 @@ public class Constituency {
 	private MultiPolygon geom;
 
 
+	@JsonIgnore
 	@JsonProperty(value="geometry")
 	public String getGeometryString() {
 		return geom.toText();
 	}
 	
-	
+
+	@JsonProperty(value="coordinates")
+	public double[][] getGeometryArray() {
+		Coordinate[] coords = geom.getCoordinates();
+		double[][] out = new double[coords.length][];
+		if(coords.length>0)
+		{
+			boolean d1 = Double.isNaN(coords[0].y), d2 = !d1 &&  Double.isNaN(coords[0].z), d3 = !d1 && !d2;
+			for(int i=0; i<coords.length; i++) {
+				out[i] = new double[d1 ? 1 : d2 ? 2 : 3];
+				out[i][0] = coords[i].x;
+				if(d2 || d3) out[i][1] = coords[i].y;
+				if(d3) out[i][2] = coords[i].z;
+			}
+		}
+		return out;
+	}
 	
 	public long getGid() {
 		return gid;
